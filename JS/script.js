8aaabc6b7fdab4bc2c225239dfd6cba8b7d96754
Peter Lauer <lauerpeter11@gmail.com>
2023-07-17 16:20:09 -0500

    added some more html and css

--- JS/script.js
+++ JS/script.js
@@ -4,47 +4,31 @@
   available here as a global variable. It was named sample in the other file so we'll use that here.
 */
 
-//GLOBAL VARIABLES
-var searchHistory = [];
+//API setup
 var weatherApiRootUrl = 'https://api.openweathermap.org';
-var weatherApiKey = 'd91f911bcf2c0f925fb6535547a5ddc9';
+var weatherApiKey = '422a727a5e93443188202765206175d1'
+
+//searched city
+var town="";
+var searchHistory = [];
+var temp
+var wind
+var humidity
 
 //DOM ELEMENTS
-var searchForm = document.querySelector('#search-button');
-var searchInput = document.querySelector('#search-input');
-var todayContainer = document.querySelector('#todayforcast');
-var forecastContainer = document.querySelector('#5dayforecast');
-var searchHistoryContainer = document.querySelector('#history');
-
-dayjs.extend(window.dayjs_plugin_utc);
-dayjs.extend(window.dayjs_plugin_timezone);
 
 
+$("#search-button").on("click", function (event) {
 
-// This is the array of hour blocks: 8 per day, for a total of 40.
-const daysInForecast = sample.list 
+  event.preventDefault();
 
-/*
-Each date object has a property called "dt", which is a Unix timestamp for the date and time 
-of that object's data. The first one is 1681333200.
-*/ 
+  q = $("#city-input").val();
+  if (q === '') {
+      return alert('Please Enter Valid City Name ! ');
+  }
+  getWeather(q);
 
-// Create a new array to hold one day block per forecast day.
-const newForecastArr = [] 
+  saveToLocalStorage(q);
 
-// iterate over the 40 blocks, but we will do them 8 at a time, so that we get one per day.
-for( let i=0; i<40; i=i+8 ){
-  newForecastArr.push( sample.list[i])
-}
+});
 
-// We now have a new array with one record for each day!
-console.log(newForecastArr)
-
-
-/* 
-Want to see why arrow functions are cool? Combined with an array method you haven't learned 
-yet, we can do all this work in one line of code. We will show you array.filter() later!
-*/ 
-
-const newForecastArr2 = sample.list.filter( (_dayObj, idx) => idx % 8 === 0)
-console.log(newForecastArr2)
